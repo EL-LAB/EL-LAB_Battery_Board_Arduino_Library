5e119a1da6b5f0588714cca37ed4de5c8ca2d252
D-Villa <d.villalobos8@gmail.com>
2018-11-04 09:41:59 -0600

    Update Battery_Read.ino

--- examples/Battery_Read/Battery_Read.ino
+++ examples/Battery_Read/Battery_Read.ino
@@ -3,14 +3,21 @@
 
     This example shows how to get the voltage and current from the battery.
 
-    This code uses the default settings which are the recommended ones:
+    This code uses the following settings which are the recommended ones for
+    voltage and current readings:
+
+    Voltage readings:
         - 16 bit resolution
         - Gain 1x
 
+    Current readings:
+        - 18 bit resolution
+        - Gain 8x
+
     Created: July 17th, 2018
     Author: El Lab de Electro: EL LAB
-    Modified: -
-    Author: -
+    Modified: November 4th, 2018
+    Author: El Lab de Electro: EL LAB
 
     URL: https://github.com/EL-LAB/EL-LAB_Battery_Board_Arduino_Library
 */
@@ -44,14 +51,22 @@ void setup()
 
 void loop()
 {
+  // Sets parameters for proper voltage reading.
+  myBattery.configureResolution(BIT_16);
+  myBattery.configureGain(GAIN_1X);
+  
   // Reads battery voltage.
   Serial.print("\nVoltage: ");
   Serial.print(myBattery.readVoltage(), 3);
   Serial.println("V");
+
+  // Sets parameters for proper current reading.
+  myBattery.configureResolution(BIT_18);
+  myBattery.configureGain(GAIN_8X);
   
   // Reads battery current.
   Serial.print("Current: ");
-  Serial.print((myBattery.readCurrent() / 1000.0), 3);
+  Serial.print(myBattery.readCurrent() * 1000.0, 3);
   Serial.println("mA");
 
   delay(5000); // Reading battery every 5 seconds.
